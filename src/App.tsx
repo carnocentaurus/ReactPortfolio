33813e5e789754cf9370a55f677f5d58280012f1
EJay <carnocentaurus@gmail.com>
2025-11-22 09:45:35 +0800

    added About.tsx in pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
@@ -16,12 +17,14 @@ function App() {
     <>
       <nav>
         <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
         <Link to="/projects">Projects</Link>
         <Link to="/contact">Contact</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
 
